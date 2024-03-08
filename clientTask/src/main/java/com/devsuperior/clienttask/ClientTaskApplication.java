bd0180b846467a6035babf924c52f161f253e8c6
Luiz Fernando <lufernandobs@gmail.com>
2024-03-08 17:34:43 -0300

    Creating Project

---
+++ clientTask/src/main/java/com/devsuperior/clienttask/ClientTaskApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.clienttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientTaskApplication.class, args);
+	}
+
+}
